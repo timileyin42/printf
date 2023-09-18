137744afa48d1d0bd9fe420e8574659eaeae314f
Abdelrhman Fikri <abdelrhmanfikri182017@gmail.com>
2023-09-18 11:53:04 -0700

    non

---
+++ print_non_printable.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_non_printable - Helper function to print non-printable char as \xXX.
+ *
+ * @buffer: buffer where the output is being constructed.
+ * @c: The character to be printed.
+ * @index: ointer to an integer that keeps track of the current position.
+*/
+void print_non_printable(char *buffer, char c, int *index)
+{
+	if (*index + 4 <= BUFFER_SIZE)
+	{
+		buffer[(*index)++] = '\\';
+		buffer[(*index)++] = 'x';
+		snprintf(buffer + *index, 3, "%02X", (unsigned char)c);
+		*index += 2;
+	}
+	else
+	{
+		write(STDOUT_FILENO, buffer, *index);
+		*index = 0;
+		print_non_printable(buffer, c, index);
+	}
+}
